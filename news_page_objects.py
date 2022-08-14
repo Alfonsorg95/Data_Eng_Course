279b2aaa29c03d5b98d452a09bb7e20a44ea39b9
Alfonso Rodriguez <Alfonsorg95@gmail.com>
2022-08-13 20:30:27 -0400

    Extranting articles data from elpais and eluniversal

--- news_page_objects.py
+++ news_page_objects.py
@@ -1,18 +1,34 @@
-from os import link
 from common import config
 import requests
 import bs4
 
 
-class HomePage:
+class NewsPage:
 
-    def __init__(self, news_site_uid, url) -> None:
+    def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
         self._queries = self._config['queries']
         self._html = None
         self._headers = {'User-Agent':'Mozilla/5.0'}
 
         self._visit(url)
+
+    
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+
+    def _visit(self, url):
+        response = requests.get(url,headers= self._headers)
+
+        response.raise_for_status()
+
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
 
     
     @property
@@ -25,13 +41,19 @@ class HomePage:
         return {link['href'] for link in link_list}
 
 
-    def _select(self, query_string):
-        return self._html.select(query_string)
+class ArticlePage(NewsPage):
 
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
 
-    def _visit(self, url):
-        response = requests.get(url,headers= self._headers)
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
 
-        response.raise_for_status()
+        return result[0].text if result else ''
 
-        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+
+        return result[0].text if result else ''
