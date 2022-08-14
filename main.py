a5ccc7676fd6c0d93d0ee548dee9b35d795562e6
Alfonso Rodriguez <Alfonsorg95@gmail.com>
2022-08-13 23:47:40 -0400

    saving articles in a csv file

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from common import config
 import logging
 import news_page_objects as news
 import re
+import datetime
+import csv
 logging.basicConfig(level=logging.INFO)
 
 
@@ -25,9 +27,22 @@ def _news_scraper(news_site_uid):
         if article:
             logger.info('Article fetched')
             articles.append(article)
-            print(article.title)
     
-    print(len(articles))
+    _save_articles(news_site_uid, articles)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%d_%m_%Y')
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(news_site_uid=news_site_uid, datetime = now)
+    csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+
+    with open(out_file_name,mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
 
 
 def _fetch_article(news_site_uid, host, link):
